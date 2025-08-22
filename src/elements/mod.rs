c508b47a79796e4e735449f3d4491d93292e75f2
Lineoo <dudu13258@163.com>
2025-08-23 00:51:23 +0800

    feat: 给 Element 添加了 Any

--- src/elements/mod.rs
+++ src/elements/mod.rs
@@ -8,3 +8,14 @@ pub trait Element: Any {
     fn name(&self) -> std::borrow::Cow<'_, str>;
     fn border(&self) -> [i32; 4];
 }
+impl dyn Element {
+    pub fn is<T: Any>(&self) -> bool {
+        (self as &dyn Any).is::<T>()
+    }
+    pub fn downcast_ref<T: Any>(&self) -> Option<&T> {
+        (self as &dyn Any).downcast_ref()
+    }
+    pub fn downcast_mut<T: Any>(&mut self) -> Option<&mut T> {
+        (self as &mut dyn Any).downcast_mut()
+    }
+}
