aafc82bcf7b9d2407bc3a307e2726c4cfe92f189
Lineoo <dudu13258@163.com>
2025-08-23 01:01:55 +0800

    tweak: 清理了 World 代码

--- src/layout/world.rs
+++ src/layout/world.rs
@@ -1,44 +1,42 @@
-use std::any::Any;
-
 use hashbrown::HashMap;
 
 use crate::elements::Element;
 
 /// Represent an element in the [`World`]. It's an handle so manual validation is needed.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ElementIdx(usize);
+pub struct ElementHandle(usize);
 
 pub struct World {
-    element_idx: ElementIdx,
-    elements: HashMap<ElementIdx, Box<dyn Element>>,
+    element_idx: ElementHandle,
+    elements: HashMap<ElementHandle, Box<dyn Element>>,
 }
 impl World {
     pub fn new() -> World {
         World {
-            element_idx: ElementIdx(0),
+            element_idx: ElementHandle(0),
             elements: HashMap::new(),
         }
     }
 
-    pub fn insert(&mut self, element: impl Element + 'static) -> ElementIdx {
+    pub fn insert(&mut self, element: impl Element + 'static) -> ElementHandle {
         self.elements.insert(self.element_idx, Box::new(element));
         self.element_idx.0 += 1;
-        ElementIdx(self.element_idx.0 - 1)
+        ElementHandle(self.element_idx.0 - 1)
     }
 
-    pub fn fetch<T: 'static>(&mut self, element_idx: ElementIdx) -> Option<&mut T> {
+    pub fn fetch<T: 'static>(&mut self, element_idx: ElementHandle) -> Option<&mut T> {
         self.elements
             .get_mut(&element_idx)
-            .and_then(|element| (element.as_mut() as &mut dyn Any).downcast_mut())
+            .and_then(|element| element.downcast_mut())
     }
 
-    pub fn fetch_dyn(&mut self, element_idx: ElementIdx) -> Option<&mut dyn Element> {
+    pub fn fetch_dyn(&mut self, element_idx: ElementHandle) -> Option<&mut dyn Element> {
         self.elements
             .get_mut(&element_idx)
             .map(|element| element.as_mut())
     }
 
-    pub fn intersect(&self, x: i32, y: i32) -> Option<ElementIdx> {
+    pub fn intersect(&self, x: i32, y: i32) -> Option<ElementHandle> {
         for (idx, element) in &self.elements {
             let border = element.border();
 
