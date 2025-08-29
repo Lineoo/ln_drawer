413253c441ffb041b16671b0899e9147e3a55446
Lineoo <dudu13258@163.com>
2025-08-29 19:41:18 +0800

    feat: World 内部可变性

--- src/layout/world.rs
+++ src/layout/world.rs
@@ -1,4 +1,8 @@
+use std::ops::{Deref, DerefMut};
+
+use hashbrown::HashSet;
 use indexmap::IndexMap;
+use parking_lot::Mutex;
 
 use crate::elements::Element;
 
@@ -9,12 +13,15 @@ pub struct ElementHandle(usize);
 pub struct World {
     element_idx: ElementHandle,
     elements: IndexMap<ElementHandle, Box<dyn Element>, hashbrown::DefaultHashBuilder>,
+
+    occupied: Mutex<HashSet<ElementHandle>>,
 }
 impl World {
     pub fn new() -> World {
         World {
             element_idx: ElementHandle(0),
             elements: IndexMap::default(),
+            occupied: Mutex::new(HashSet::new()),
         }
     }
 
@@ -50,6 +57,39 @@ impl World {
             .map(|element| element.as_mut())
     }
 
+    // TODO not panic pls
+    // TODO move fetch codes to the guard
+
+    pub fn fetch_cell<T: 'static>(&self, element_idx: ElementHandle) -> Option<WorldCell<'_, T>> {
+        let mut occupied = self.occupied.lock();
+
+        assert!(!occupied.contains(&element_idx), "{element_idx:?} occupied");
+        let element = self.elements.get(&element_idx)?.downcast_ref()? as *const T;
+        occupied.insert(element_idx);
+
+        Some(WorldCell {
+            // Cast safety: only one access is allowed
+            ptr: element as *mut T,
+            world: self,
+            idx: element_idx,
+        })
+    }
+
+    pub fn fetch_cell_dyn(&self, element_idx: ElementHandle) -> Option<WorldCell<'_, dyn Element>> {
+        let mut occupied = self.occupied.lock();
+
+        assert!(!occupied.contains(&element_idx), "{element_idx:?} occupied");
+        let element = self.elements.get(&element_idx)?.as_ref() as *const dyn Element;
+        occupied.insert(element_idx);
+
+        Some(WorldCell {
+            // Cast safety: only one access is allowed
+            ptr: element as *mut dyn Element,
+            world: self,
+            idx: element_idx,
+        })
+    }
+
     pub fn intersect(&self, x: i32, y: i32) -> Option<ElementHandle> {
         for (idx, element) in &self.elements {
             let border = element.get_border();
@@ -79,5 +119,30 @@ impl World {
 
     pub fn foreach<T: Element>(&self) -> impl Iterator<Item = &T> {
         (self.elements.values()).filter_map(|element| element.downcast_ref::<T>())
+    }
+}
+
+pub struct WorldCell<'world, T: ?Sized> {
+    ptr: *mut T,
+    world: &'world World,
+    idx: ElementHandle,
+}
+impl<T: ?Sized> Deref for WorldCell<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        // SAFETY: guaranteed by World's cell_occupied
+        unsafe { self.ptr.as_ref().unwrap() }
+    }
+}
+impl<T: ?Sized> DerefMut for WorldCell<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // SAFETY: guaranteed by World's cell_occupied
+        unsafe { self.ptr.as_mut().unwrap() }
+    }
+}
+impl<T: ?Sized> Drop for WorldCell<'_, T> {
+    fn drop(&mut self) {
+        let mut occupied = self.world.occupied.lock();
+        occupied.remove(&self.idx);
     }
 }
