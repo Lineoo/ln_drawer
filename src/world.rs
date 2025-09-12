2ea072c10f3dbda0b9c9986f96aaf5a74c5df744
Lineoo <dudu13258@163.com>
2025-09-12 20:56:15 +0800

    feat: WorldCell 修复&大改

--- src/world.rs
+++ src/world.rs
@@ -4,7 +4,7 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-use hashbrown::{DefaultHashBuilder, HashMap, HashSet};
+use hashbrown::{DefaultHashBuilder, HashMap};
 use indexmap::IndexMap;
 use parking_lot::Mutex;
 
@@ -15,13 +15,11 @@ use crate::elements::Element;
 pub struct ElementHandle(usize);
 
 #[derive(Default)]
+#[expect(clippy::type_complexity)]
 pub struct World {
     curr_idx: ElementHandle,
     elements: IndexMap<ElementHandle, Box<dyn Element>, DefaultHashBuilder>,
     observers: HashMap<ElementHandle, Vec<Box<dyn FnMut(&dyn Any, &mut World)>>>,
-
-    // FIXME: in move
-    occupied: Mutex<HashSet<ElementHandle>>,
 }
 impl World {
     pub fn insert(&mut self, element: impl Element + 'static) -> ElementHandle {
@@ -64,38 +62,11 @@ impl World {
             .map(|element| element.as_mut())
     }
 
-    // TODO not panic pls
-    // TODO move fetch codes to the guard
-    // FIXME immutable reference should be unaccessible while cell is fetched (New: WorldMutex)
-
-    pub fn fetch_cell<T: Element>(&self, handle: ElementHandle) -> Option<WorldCell<'_, T>> {
-        let mut occupied = self.occupied.lock();
-
-        assert!(!occupied.contains(&handle), "{handle:?} occupied");
-        let element = self.elements.get(&handle)?.downcast_ref()? as *const T;
-        occupied.insert(handle);
-
-        Some(WorldCell {
-            // Cast safety: only one access is allowed
-            ptr: element as *mut T,
+    pub fn cell(&mut self) -> WorldCell<'_> {
+        WorldCell {
             world: self,
-            idx: handle,
-        })
-    }
-
-    pub fn fetch_cell_dyn(&self, handle: ElementHandle) -> Option<WorldCell<'_, dyn Element>> {
-        let mut occupied = self.occupied.lock();
-
-        assert!(!occupied.contains(&handle), "{handle:?} occupied");
-        let element = self.elements.get(&handle)?.as_ref() as *const dyn Element;
-        occupied.insert(handle);
-
-        Some(WorldCell {
-            // Cast safety: only one access is allowed
-            ptr: element as *mut dyn Element,
-            world: self,
-            idx: handle,
-        })
+            occupied: Mutex::new(HashMap::new()),
+        }
     }
 
     pub fn entry<T: Element>(&mut self, handle: ElementHandle) -> WorldElement<'_, T> {
@@ -186,29 +157,202 @@ impl World {
     }
 }
 
-/// A world's limitedly mutable element reference.
-pub struct WorldCell<'world, T: ?Sized> {
-    ptr: *mut T,
-    world: &'world World,
-    idx: ElementHandle,
+// Center of multiple accesses in world
+pub struct WorldCell<'world> {
+    world: &'world mut World,
+    occupied: Mutex<HashMap<ElementHandle, isize>>,
 }
-impl<T: ?Sized> Deref for WorldCell<'_, T> {
+impl WorldCell<'_> {
+    pub fn fetch<T: Element>(&self, handle: ElementHandle) -> Option<Ref<'_, T>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt < 0 {
+            panic!("{handle:?} is mutably borrowed");
+        }
+
+        *cnt += 1;
+        let element = self.world.elements.get(&handle)?.downcast_ref()?;
+
+        Some(Ref {
+            ptr: element as *const T,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn fetch_dyn(&self, handle: ElementHandle) -> Option<Ref<'_, dyn Element>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt < 0 {
+            panic!("{handle:?} is mutably borrowed");
+        }
+
+        *cnt += 1;
+        let element = self.world.elements.get(&handle)?.as_ref();
+
+        Some(Ref {
+            ptr: element as *const dyn Element,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn fetch_mut<T: Element>(&self, handle: ElementHandle) -> Option<RefMut<'_, T>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt != 0 {
+            panic!("{handle:?} is borrowed");
+        }
+
+        *cnt -= 1;
+        let element = self.world.elements.get(&handle)?.downcast_ref()?;
+
+        Some(RefMut {
+            ptr: element as *const T as *mut T,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn fetch_mut_dyn(&mut self, handle: ElementHandle) -> Option<RefMut<'_, dyn Element>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt != 0 {
+            panic!("{handle:?} is borrowed");
+        }
+
+        *cnt -= 1;
+        let element = self.world.elements.get(&handle)?.as_ref();
+
+        Some(RefMut {
+            ptr: element as *const dyn Element as *mut dyn Element,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn try_fetch<T: Element>(&self, handle: ElementHandle) -> Option<Ref<'_, T>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt < 0 {
+            return None;
+        }
+
+        *cnt += 1;
+        let element = self.world.elements.get(&handle)?.downcast_ref()?;
+
+        Some(Ref {
+            ptr: element as *const T,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn try_fetch_dyn(&self, handle: ElementHandle) -> Option<Ref<'_, dyn Element>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt < 0 {
+            return None;
+        }
+
+        *cnt += 1;
+        let element = self.world.elements.get(&handle)?.as_ref();
+
+        Some(Ref {
+            ptr: element as *const dyn Element,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn try_fetch_mut<T: Element>(&self, handle: ElementHandle) -> Option<RefMut<'_, T>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt != 0 {
+            return None;
+        }
+
+        *cnt -= 1;
+        let element = self.world.elements.get(&handle)?.downcast_ref()?;
+
+        Some(RefMut {
+            ptr: element as *const T as *mut T,
+            world: self,
+            handle,
+        })
+    }
+
+    pub fn try_fetch_mut_dyn(&mut self, handle: ElementHandle) -> Option<RefMut<'_, dyn Element>> {
+        let mut occupied = self.occupied.lock();
+
+        let cnt = occupied.entry(handle).or_default();
+        if *cnt != 0 {
+            return None;
+        }
+
+        *cnt -= 1;
+        let element = self.world.elements.get(&handle)?.as_ref();
+
+        Some(RefMut {
+            ptr: element as *const dyn Element as *mut dyn Element,
+            world: self,
+            handle,
+        })
+    }
+}
+
+/// A world's immutable element reference.
+pub struct Ref<'world, T: ?Sized> {
+    ptr: *const T,
+    world: &'world WorldCell<'world>,
+    handle: ElementHandle,
+}
+impl<T: ?Sized> Deref for Ref<'_, T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         // SAFETY: guaranteed by World's cell_occupied
         unsafe { self.ptr.as_ref().unwrap() }
     }
 }
-impl<T: ?Sized> DerefMut for WorldCell<'_, T> {
+impl<T: ?Sized> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        let mut occupied = self.world.occupied.lock();
+        let cnt = occupied.get_mut(&self.handle).unwrap();
+        *cnt -= 1;
+    }
+}
+
+/// A world's limitedly mutable element reference.
+pub struct RefMut<'world, T: ?Sized> {
+    ptr: *mut T,
+    world: &'world WorldCell<'world>,
+    handle: ElementHandle,
+}
+impl<T: ?Sized> Deref for RefMut<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        // SAFETY: guaranteed by World's cell_occupied
+        unsafe { self.ptr.as_ref().unwrap() }
+    }
+}
+impl<T: ?Sized> DerefMut for RefMut<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         // SAFETY: guaranteed by World's cell_occupied
         unsafe { self.ptr.as_mut().unwrap() }
     }
 }
-impl<T: ?Sized> Drop for WorldCell<'_, T> {
+impl<T: ?Sized> Drop for RefMut<'_, T> {
     fn drop(&mut self) {
         let mut occupied = self.world.occupied.lock();
-        occupied.remove(&self.idx);
+        let cnt = occupied.get_mut(&self.handle).unwrap();
+        *cnt += 1;
     }
 }
 
@@ -227,17 +371,6 @@ impl<T: Element> Deref for WorldElement<'_, T> {
 impl<T: Element> DerefMut for WorldElement<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.world.fetch_mut(self.handle).unwrap()
-    }
-}
-impl Deref for WorldElement<'_, dyn Element> {
-    type Target = dyn Element;
-    fn deref(&self) -> &Self::Target {
-        self.world.fetch_dyn(self.handle).unwrap()
-    }
-}
-impl DerefMut for WorldElement<'_, dyn Element> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.world.fetch_mut_dyn(self.handle).unwrap()
     }
 }
 impl<T: Element> WorldElement<'_, T> {
@@ -263,5 +396,16 @@ impl<T: Element> WorldElement<'_, T> {
 
             self.world.observers.insert(self.handle, observers);
         }
+    }
+}
+impl Deref for WorldElement<'_, dyn Element> {
+    type Target = dyn Element;
+    fn deref(&self) -> &Self::Target {
+        self.world.fetch_dyn(self.handle).unwrap()
+    }
+}
+impl DerefMut for WorldElement<'_, dyn Element> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.world.fetch_mut_dyn(self.handle).unwrap()
     }
 }
