ff15ba6068e37ca0822dc668cae4437a47efdc78
Lineoo <dudu13258@163.com>
2025-10-18 23:28:57 +0800

    node: NodeLinks 健壮

--- src/tools/node.rs
+++ src/tools/node.rs
@@ -2,34 +2,63 @@ use std::marker::PhantomData;
 
 use hashbrown::HashMap;
 
-use crate::world::{Destroy, Element, ElementHandle, WorldCell};
+use crate::world::{Destroy, Element, ElementHandle, InsertElement, WorldCell};
 
 pub struct NodeLinks<P> {
-    invert: HashMap<ElementHandle, ElementHandle>,
+    record: HashMap<ElementHandle, Link>,
     _marker: PhantomData<P>,
+}
+
+struct Link {
+    from: ElementHandle,
+    to: ElementHandle,
+    from_ob: ElementHandle,
+    to_ob: ElementHandle,
 }
 
 impl<P: 'static> NodeLinks<P> {
     pub fn build_link(&mut self, from: ElementHandle, to: ElementHandle, world: &WorldCell) {
         if let (Some(from_entry), Some(to_entry)) = (world.entry(from), world.entry(to)) {
-            self.invert.insert(to, from);
-
-            let from_ob = from_entry.observe(move |Destroy, entry| {
-                let mut links = entry.single_fetch_mut::<NodeLinks<P>>().unwrap();
-                links.invert.remove(&to);
+            let from_ob = from_entry.observe(move |Destroy, world| {
+                let mut links = world.single_fetch_mut::<NodeLinks<P>>().unwrap();
+                links.remove_link(to, &world);
             });
 
-            let to_ob = to_entry.observe(move |Destroy, entry| {
-                let mut links = entry.single_fetch_mut::<NodeLinks<P>>().unwrap();
-                links.invert.remove(&to);
+            let to_ob = to_entry.observe(move |Destroy, world| {
+                let mut links = world.single_fetch_mut::<NodeLinks<P>>().unwrap();
+                links.remove_link(to, &world);
             });
 
-            world.entry(to_ob).unwrap().depend(from_ob);
-            world.entry(from_ob).unwrap().depend(to_ob);
+            let prev = self.record.insert(
+                to,
+                Link {
+                    from,
+                    to,
+                    from_ob,
+                    to_ob,
+                },
+            );
+
+            if let Some(prev) = prev {
+                world.remove(prev.from_ob);
+                world.remove(prev.to_ob);
+            }
         } else {
-            log::error!("{from} or {to} does not exist when attempting to link them");
+            log::error!("either {from} or {to} does not exist when attempting to link them");
         }
+    }
+
+    pub fn remove_link(&mut self, to: ElementHandle, world: &WorldCell) {
+        if let Some(link) = self.record.remove(&to) {
+            world.remove(link.from_ob);
+            world.remove(link.to_ob);
+        }
+    }
+
+    pub fn get_link(&self, to: ElementHandle) -> Option<ElementHandle> {
+        self.record.get(&to).map(|link| link.from)
     }
 }
 
 impl<P: 'static> Element for NodeLinks<P> {}
+impl<P: 'static> InsertElement for NodeLinks<P> {}
