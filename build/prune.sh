42428e54eecae7893fca5b4691eace99488bdd02
Lineoo <dudu13258@163.com>
2026-03-30 21:39:17 +0800

    icon: 更新了 prune 脚本

--- build/prune.sh
+++ build/prune.sh
@@ -4,8 +4,18 @@ set -e
 
 cp Cargo.toml Cargo.toml.old
 
+# [package.metadata.android]
+# version = "0.0.0-alpha.0-dev"
 sed -i '/^\[package\]/,/^\[/ s/^version = "\(.*\)-dev"$/version = "\1"/' Cargo.toml
 
+# [package.metadata.android]
+# package = "dev.linn.lndrawer"
 sed -i '/^\[package\.metadata\.android\]/,/^\[/ s/^package = "dev\.\(.*\)"$/package = "org.\1"/' Cargo.toml
 
+# [package.metadata.android.application]
+# icon = "LnDrawer Dev"
 sed -i '/^\[package\.metadata\.android\.application\]/,/^\[/ s/^label = "\(.*\) Dev"$/label = "\1"/' Cargo.toml
+
+# [package.metadata.android.application]
+# icon = "@mipmap/icon-dev"
+sed -i '/^\[package\.metadata\.android\.application\]/,/^\[/ s/^icon = "\(.*\)-dev"$/icon = "\1"/' Cargo.toml
