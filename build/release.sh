d7d616287f043f4a5a0de53f6f4c498d833d16f3
Lineoo <dudu13258@163.com>
2026-04-06 22:47:26 +0800

    改进了发布脚本

--- build/release.sh
+++ build/release.sh
@@ -7,12 +7,14 @@ if [ ! -f "./Cargo.toml.old" ]; then
     exit
 fi
 
-echo "Compiling for x86_64-unknown-linux-gnu..."
+echo -e "\033[34m:: Compiling for x86_64-unknown-linux-gnu...\033[0m"
 cargo build --release --target x86_64-unknown-linux-gnu
 
-echo "Compiling for x86_64-pc-windows-gnu..."
+echo -e "\033[34m:: Compiling for x86_64-pc-windows-gnu...\033[0m"
 cargo build --release --target x86_64-pc-windows-gnu
 
-echo "Compiling for Android (cargo-apk)..."
+echo -e "\033[34m:: Compiling for Android (cargo-apk)...\033[0m"
 source ~/org.linn.keyconf
-cargo apk build --release --lib
+cargo apk build --release --lib --target aarch64-linux-android
+
+echo -e "\033[32m:: Compiling finished.\033[0m"
