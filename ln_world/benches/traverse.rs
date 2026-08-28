a3e0bbcd8e7d29149bdd8f51e68f04635e56bd38
Lineoo <dudu13258@163.com>
2026-08-28 16:56:38 +0800

    add benchmark for traverse

---
+++ ln_world/benches/traverse.rs
@@ -0,0 +1,79 @@
+#![feature(test)]
+
+extern crate test;
+
+use ln_world::{Element, World};
+use test::Bencher;
+
+struct Camera;
+struct RenderControl;
+
+impl Element for Camera {}
+impl Element for RenderControl {}
+
+#[bench]
+fn traverse1x100(b: &mut Bencher) {
+    let world = setup_world(1, 100);
+    b.iter(|| traverse_control(&world));
+}
+
+#[bench]
+fn traverse2x100(b: &mut Bencher) {
+    let world = setup_world(2, 100);
+    b.iter(|| traverse_control(&world));
+}
+
+#[bench]
+fn traverse5x100(b: &mut Bencher) {
+    let world = setup_world(5, 100);
+    b.iter(|| traverse_control(&world));
+}
+
+#[bench]
+fn traverse2x50(b: &mut Bencher) {
+    let world = setup_world(2, 50);
+    b.iter(|| traverse_control(&world));
+}
+
+#[bench]
+fn traverse5x20(b: &mut Bencher) {
+    let world = setup_world(5, 20);
+    b.iter(|| traverse_control(&world));
+}
+
+#[bench]
+fn traverse10x10(b: &mut Bencher) {
+    let world = setup_world(10, 10);
+    b.iter(|| traverse_control(&world));
+}
+
+#[bench]
+fn traverse50x2(b: &mut Bencher) {
+    let world = setup_world(50, 2);
+    b.iter(|| traverse_control(&world));
+}
+
+fn traverse_control(world: &World) {
+    world.foreach_enter::<Camera>(|camera| {
+        world.foreach_fetch::<RenderControl>(|control| {
+            test::black_box((camera, control));
+        });
+    });
+}
+
+fn setup_world(camera: usize, control: usize) -> World {
+    let mut world = World::new();
+
+    for _ in 0..camera {
+        let camera = world.insert(Camera);
+        world.enter(camera, || {
+            for _ in 0..control {
+                world.insert(RenderControl);
+            }
+        });
+    }
+
+    // setup
+    world.flush();
+    world
+}
